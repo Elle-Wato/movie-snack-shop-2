379019364a67ccd8963a0285c0ec6717dfcb5e25
Wato Elle <watoelle@gmail.com>
2024-09-18 12:44:43 +0300

    updated

---
+++ src/Components/TicketItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function TicketItem({ ticket, addToCart }) {
+  return (
+    <div className="item ticket-item">
+      <img src={ticket.poster} alt={ticket.title} />
+      <h3>{ticket.title}</h3>
+      <p>Showtime: {ticket.showtime}</p>
+      <p>Runtime: {ticket.runtime} minutes</p>
+      <p>Description: {ticket.description}</p>
+      <button onClick={() => addToCart(ticket)}>Add to Cart</button>
+    </div>
+  );
+}
+
+export default TicketItem;
